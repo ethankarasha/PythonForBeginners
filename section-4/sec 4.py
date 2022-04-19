f948fa2591638d48be715c4391547d7b4df71f9d
ethankarasha <102565724+ethankarasha@users.noreply.github.com>
2022-04-18 17:36:45 -0700

    Revert "Revert "Sections 3 and 4""
    This reverts commit 12cdb04d02054d2ab411299dbd2d1ccb3f4c5e32.

---
+++ section-4/sec 4.py
@@ -0,0 +1,46 @@
+def hello_world_printer():
+    print("hello world")
+
+
+hello_world_printer()
+#name = input("What is your name? ")
+
+
+#def name_printer(p1):
+    #print(p1)
+
+
+    #name_printer(name)
+
+
+ 
+#length = int(input("enter a number for length(in feet)"))
+#width = int(input("enter a number for width(in feet)"))
+#height = int(input("enter a number for height(in feet)"))
+
+#def volume(length, width, height):
+    #return length * width * height
+
+
+#print("The volume of this rectangular prism is " + str(volume(length, width, height)) + " cubic feet.")
+
+
+#celsius = int(input("Enter a temperature in Celsius"))
+
+
+#def fahrenheit(celsius):
+    #return (18 * celsius + 320) / 10
+
+
+#print("The Fahrenheit equivalent of " + str(celsius) + " degrees Celsius is " + str(fahrenheit(celsius)) + "." )
+
+
+
+from random import randint
+
+gallons = randint(10,25)
+miles = randint(200,400)
+
+print("The car's MPG is " + str(miles//gallons) + ".")
+print("The number of gallons being held in the the car is " + str(gallons) + ".")
+print("The number of miles the car traveled is " + str(miles) + ".")
